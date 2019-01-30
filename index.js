84c1a58a3d192c65d6e7a51d63def53edff4bc10
Aaron Feinberg <aaron.feinberg@wework.com>
2019-01-30 02:59:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-function theBeatlesPlay(musicians,instruments){
-  var array = [];
-  for (var i = 0; i < musicians.length; i++) {
-    array(musicians[i] + "plays" + instruments[i])
-  }
-  return array
-  }
+
 
   
